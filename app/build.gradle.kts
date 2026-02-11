38cd3608e7c16c89d4b9327e31bce5e1cba0f6a0
codingnoob215 <malcolmkenyatta1@yahoo.com>
2026-02-11 15:29:43 -0500

    camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,12 @@ dependencies {
     implementation("org.tensorflow:tensorflow-lite:2.14.0")
     implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
     implementation("org.tensorflow:tensorflow-lite-gpu:2.14.0")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    // CameraX
+    implementation("androidx.camera:camera-core:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-view:1.3.4")
 }
